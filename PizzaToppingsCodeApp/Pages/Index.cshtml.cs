819550f6442a559e5e12846e9e59d51a69b110a6
Harrison Hess <harrisonhess1@aol.com>
2022-03-01 22:51:55 -0500

    1st

--- PizzaToppingsCodeApp/Pages/Index.cshtml.cs
+++ PizzaToppingsCodeApp/Pages/Index.cshtml.cs
@@ -24,8 +24,9 @@ namespace PizzaToppingsCodeApp.Pages
             var client = new WebClient();
             var json = client.DownloadString("https://www.brightway.com/CodeTests/pizzas.json");
             var pizzas = JsonConvert.DeserializeObject<Pizza[]>(json);
+            var pieData = new List<Pizza>();
 
-            int count = pizzas.Length;
+            //int count = pizzas.Length;
             List<string> allToppings = new List<string>();
                        
             foreach (Pizza p in pizzas)
@@ -34,10 +35,22 @@ namespace PizzaToppingsCodeApp.Pages
                 {
                     allToppings.Add(s);
                 }
+                pieData = pizzas.GroupBy(x => x)
+                                     .Where(y => y.Count() > 1)
+                                     .Select(z => z.Key)
+                                     .ToList();
             }
 
-            var toppingData = new List<string>(allToppings.OrderByDescending(x => x.Count()));
-            var pieData = new List<string>(pizzas.);
+            //get count of each unique topping and order by count
+            //find duplicate pizzas and order by count
+
+            var toppingData = new List<string>(allToppings);
+            toppingData = toppingData.GroupBy(x => x)
+                                     .Where(y => y.Count() > 1)
+                                     .Select(z => z.Key)
+                                     .ToList();
+            //var pieData = new List<string>(!pizzas.Distinct());
+            Console.WriteLine(pizzas[0].toppings.SequenceEqual(pizzas[1].toppings));
 
             ViewData["uniques"] = toppingData;
             ViewData["uniquePies"] = pieData;
